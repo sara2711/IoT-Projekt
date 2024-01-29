e5cf4e8a5f7cb2b32a6eb6e92e864a3cb7be5422
sara2711 <158043707+sara2711@users.noreply.github.com>
2024-01-29 18:50:45 +0100

    Add files via upload

---
+++ pushNachricht.ino
@@ -0,0 +1,42 @@
+#include <WiFi.h> 
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+#define BOT_TOKEN "6681100756:AAHRdaLOORGJEBF4vANEGaa-1o5u1gW_9Sc"
+//#define CHATID "6939401605"
+#define CHATID "-4125811031"  //group ID
+
+#define TELEGRAMTEXT "Hallo"
+
+WiFiClientSecure secured_client; 
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
+const char* ssid     = "LV-PK";
+const char* password = "wlMDsvlVKr6aAc0cQS";
+
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(115200);
+
+  Serial.println("Telegram Bot");
+  Serial.println();
+  Serial.print("[WiFi] Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); 
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay (500);
+  }
+
+  bot.sendMessage(CHATID, TELEGRAMTEXT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
